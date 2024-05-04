ab72c35f7dac690cdd86ef02db6309142ec72439
Joan Cerepi <cyberstriker27@proton.me>
2024-05-04 15:34:17 -0600

    week 2 assignment: temple album

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const hamMenu = document.getElementById("hamMenu");
+const navigation = document.querySelector(".navigation");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("change");
+  navigation.classList.toggle("show-links");
+});
